b525cedbd989d798f9b2b8d077284f9e87aeec4d
Akanksh Suhas Divyananda <68886977+akankshd@users.noreply.github.com>
2023-06-17 12:56:02 -0700

    test

--- src/handlers/ping.js
+++ src/handlers/ping.js
@@ -3,7 +3,7 @@ import { success } from '../helpers/general';
 module.exports.ping = async evt => {
 
     return success({
-        message: 'Hello from Vibescope server!',
+        message: 'Hello from Test server!',
         input: evt.queryStringParameters
     });
 };
